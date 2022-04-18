9dc3a1deff125f13cdcda8ab36ce3262e42fc199
Lucas de Souza Reis <101409815+lucasreis741@users.noreply.github.com>
2022-04-18 16:13:36 -0300

    Add files via upload

---
+++ IMC.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void  main()
+{
+float peso,altura, imc;
+	
+printf("Entre com o seu peso :\n");
+	scanf("%f", &peso) ; 
+	
+printf("Entre com a sua altura :\n");
+	scanf("%f" , &altura) ;
+		
+	imc =  peso/altura*altura;
+	
+	printf("Seu IMC vale %f\n", imc);
+	
+	return 0 ;
+}
